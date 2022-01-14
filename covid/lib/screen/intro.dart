686b32aa2fd814b5d6aa4fafbc721ae4aeb40355
shweta kc <shweta.kc016@gmail.com>
2022-01-14 17:42:23 +0545

    Add description of covid

--- covid/lib/screen/intro.dart
+++ covid/lib/screen/intro.dart
@@ -11,27 +11,48 @@ class _IntroState extends State<Intro> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          AppColors.mainColor,
-          AppColors.mainColor.withOpacity(0.5)
-        ],
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        ),
-        ),
-       child: Stack(children:<Widget>[
-         Padding(padding: const EdgeInsets.only(top: 50),
-         child: Align(
-           alignment: Alignment.topCenter,
-           child: Image.asset('assets/images/logo.png'),
-         ),
-         ),
-       ], 
-       )
-      ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                AppColors.mainColor,
+                AppColors.mainColor.withOpacity(0.5)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+              ),
+              Positioned(
+                  bottom: 50,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Corona Virus disease (COVID-19)',
+                          style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                         Text(
+                          'is an infection disease caused by a new/virus',
+                          style: TextStyle(fontSize: 24, color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ))
+            ],
+          )),
     );
   }
 }
