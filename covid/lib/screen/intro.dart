6fb9f7a66a3d440c21635e255aba4c4a389d6580
shweta kc <shweta.kc016@gmail.com>
2022-01-14 17:32:43 +0545

    Add color to whole page

--- covid/lib/screen/intro.dart
+++ covid/lib/screen/intro.dart
@@ -1,7 +1,7 @@
+import 'package:covid/core/contst.dart';
 import 'package:flutter/material.dart';
 
 class Intro extends StatefulWidget {
-
   @override
   _IntroState createState() => _IntroState();
 }
@@ -10,7 +10,20 @@ class _IntroState extends State<Intro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          AppColors.mainColor,
+          AppColors.mainColor.withOpacity(0.5)
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        ),
+        ),
+        
+      ),
     );
   }
 }
