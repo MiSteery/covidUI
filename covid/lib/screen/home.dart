1f15df45d399f71ee10187cb9a1b593a107577d6
shweta kc <shweta.kc016@gmail.com>
2022-01-17 21:16:58 +0545

    Add build header

--- covid/lib/screen/home.dart
+++ covid/lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:covid/core/contst.dart';
 import 'package:covid/core/flutter_icon.dart';
+import 'package:covid/widget/customeAppbar.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,133 +12,108 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-              color: AppColors.mainColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              )),
-          child: Column(
-            children: <Widget>[
-              buildAppBar(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      'COVID -19',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      'Coronavirus Relief fund',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      'to this fund will help to stop the virus spread and give\n communities of the font lines.',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          height: 1.5),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: RaisedButton(
-                          color: Colors.blue,
-                          onPressed: () {},
-                          child: Text(
-                            'DONATE NOW',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(16),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 20,),
-                         Expanded(
-                        child: RaisedButton(
-                          color: Colors.red,
-                          onPressed: () {},
-                          child: Text(
-                            'EMERGENCY',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(16),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ],
-          ),
-        )
-      ],
-    ));
-  }
-
-  Widget buildAppBar() {
-    return Padding(
-      padding: EdgeInsets.only(top: 25),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(
-              FlutterIcons.menu,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
+      body: Column(
+        children: [
           Container(
-            width: 50,
-            height: 50,
-            margin: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(100),
-                ),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/profile.jpg'),
-                )),
-          )
+              decoration: BoxDecoration(
+                  color: AppColors.mainColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  )),
+              child: Stack(
+                children: <Widget>[
+                  buildHeader(),
+                ],
+              ))
         ],
       ),
+    );
+  }
+
+  Widget buildHeader() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        CustomAppBarWidget(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'COVID -19',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'Coronavirus Relief fund',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'to this fund will help to stop the virus spread and give\n communities of the font lines.',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, height: 1.5),
+          ),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: RaisedButton(
+                color: Colors.blue,
+                onPressed: () {},
+                child: Text(
+                  'DONATE NOW',
+                  style: TextStyle(color: Colors.white),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 16),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: RaisedButton(
+                color: Colors.red,
+                onPressed: () {},
+                child: Text(
+                  'EMERGENCY',
+                  style: TextStyle(color: Colors.white),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 16),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
