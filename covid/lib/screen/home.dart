5d3ab9a861b4ef786376ba99bf5c6ae6cd3b1f9a
shweta kc <shweta.kc016@gmail.com>
2022-01-17 11:36:44 +0545

    Add profile picture

--- covid/lib/screen/home.dart
+++ covid/lib/screen/home.dart
@@ -21,17 +21,30 @@ class _HomeState extends State<Home> {
                 bottomRight: Radius.circular(20),
               )),
           child: Column(
-            children: [
+            children:<Widget> [
               Padding(
                 padding: EdgeInsets.only(top: 25),
                 child: Row(
-                  children: [
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:<Widget> [
                     IconButton(                     
                       icon: Icon(
                         FlutterIcons.menu,
                         color: Colors.white,
                       ),
                        onPressed: () {},
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      margin: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(100),
+                        ),
+                        image: DecorationImage(image: AssetImage('assets/images/profile.jpg'),)
+                      ),
                     )
                   ],
                 ),
