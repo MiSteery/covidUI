f0f5d18d1379dd95eaf42dec9efcf56714b156cc
shweta kc <shweta.kc016@gmail.com>
2022-01-17 10:41:22 +0545

    change formating

--- covid/lib/screen/intro.dart
+++ covid/lib/screen/intro.dart
@@ -25,66 +25,75 @@ class _IntroState extends State<Intro> {
           ),
           child: Stack(
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-              ),
-              Positioned(
-                  bottom: 50,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Corona Virus disease (COVID-19)',
-                          style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          'is an infection disease caused by a new/virus',
-                          style: TextStyle(fontSize: 24, color: Colors.white),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          height: 25,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(50),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black38,
-                                  offset: Offset(1, 1),
-                                  spreadRadius: 1,
-                                  blurRadius: 3,
-                                )
-                              ]),
-                          width: MediaQuery.of(context).size.width * .85,
-                          height: 60,
-                          child: Center(
-                            child: Text(
-                              'GET STARTED',
-                              style: TextStyle(
-                                  color: AppColors.mainColor,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ))
+              buildHeader(),
+              buildFooter(context),
             ],
           )),
+    );
+  }
+
+  Widget buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 50),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Image.asset('assets/images/logo.png'),
+      ),
+    );
+  }
+
+  Widget buildFooter(BuildContext context) {
+    return Positioned(
+      bottom: 50,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Corona Virus disease (COVID-19)',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'is an infection disease caused by a new/virus',
+              style: TextStyle(fontSize: 24, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black38,
+                      offset: Offset(1, 1),
+                      spreadRadius: 1,
+                      blurRadius: 3,
+                    )
+                  ]),
+              width: MediaQuery.of(context).size.width * .85,
+              height: 60,
+              child: Center(
+                child: Text(
+                  'GET STARTED',
+                  style: TextStyle(
+                      color: AppColors.mainColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
