813fb061fab29adc36fd5405864513cacfd4719c
shweta kc <shweta.kc016@gmail.com>
2022-01-18 16:48:19 +0545

    Add statistic page

---
+++ covid/lib/screen/statistic.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StatisticPage extends StatefulWidget {
+
+  @override
+  _StatisticPageState createState() => _StatisticPageState();
+}
+
+class _StatisticPageState extends State<StatisticPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('data'),
+    );
+  }
+}
